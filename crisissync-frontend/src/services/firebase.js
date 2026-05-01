f4bbbf701efe489b36e785117303d33fbab0ede5
jishnu <vjishnu744@gmail.com>
2026-05-01 14:57:13 +0530

    fix: use env variable for firebase api key

--- crisissync-frontend/src/services/firebase.js
+++ crisissync-frontend/src/services/firebase.js
@@ -3,9 +3,12 @@ import { getAuth, signInAnonymously } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDgku8CripjFp8JMEA1aol-Gmqe5XH25PI",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,  // ← change this line
   authDomain: "crisissync-codecrafters.firebaseapp.com",
   projectId: "crisissync-codecrafters",
+  storageBucket: "crisissync-codecrafters.firebasestorage.app",
+  messagingSenderId: "349274111564",
+  appId: "1:349274111564:web:f3c5a2470dbe84e58abee9",
 };
 
 const app  = initializeApp(firebaseConfig);
